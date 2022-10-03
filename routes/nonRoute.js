1f3c298722968efe0429063891b802bee950236a
solomonbarayev <sbarayev2@gmail.com>
2022-10-03 21:24:26 +0300

    add backend files

--- routes/nonRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-
-router.get('*', (req, res) => {
-  res.status(404).send({ message: 'Requested resource not found' });
-});
-
-module.exports = router;
--- utils/regex.js
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line operator-linebreak
-const urlRegex =
-  /[(http(s)?)://(www.)?a-zA-Z0-9@:%.+~#=]{2,256}.[a-z]{2,6}([-a-zA-Z0-9@:%+.~#?&//=]*)/gi;
-
-module.exports = urlRegex;
